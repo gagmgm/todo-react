aa99b214a8f28b0f36bc2ef95e61d89c5acfabcc
Gagik Minasyan <gagmgm@gmail.com>
2019-10-01 12:44:28 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListItem from './components/ListItem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="page__wrapper">
+            <h1 className="text-center pt-4 mb-3">To Do List</h1>
+            <div className="text-center mb-4">
+                <button className="btn-primary" id="btn-create-todo">Create ToDo</button>
+            </div>
+            <div id="edit-area-wrapper">
+                <button id="close-popup" className="btn btn-close">x</button>
+                <div className='edit-area'>
+                    <div className="form-group">
+                        <label htmlFor="title-text">Title</label>
+                        <input type="text" id="title-text" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="textarea">Description</label>
+                        <textarea id="textarea"></textarea>
+                    </div>
+                    <button className="btn-primary" id="btn-submit">Submit</button>
+                </div>
+            </div>
+            <div className="container-fluid">
+                <div className="to-do-list d-flex">
+                    <div className="list-item">
+                        <h2 id="to-do-title">to do</h2>
+                        <ListItem elemId="todo-item-area"/>
+                    </div>
+                    <div className="list-item">
+                        <h2>in progress</h2>
+                        <ListItem elemId="inprogress-item-area"/>
+                    </div>
+                    <div className="list-item">
+                        <h2>done</h2>
+                        <ListItem elemId="done-item-area"/>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
   );
 }
---
+++ src/components/ListItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ListItem (props) {
+	return (
+		<div className="list-item-area" id={props.elemId}></div>
+	)
+}
+
+export default ListItem;
